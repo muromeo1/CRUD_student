7782738ae586a2be2f38669f0699f4f43ebebd83
Murilo Romeo <muriloh65@gmail.com>
2018-08-24 14:29:55 -0300

    Adding the complete crud

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,12 @@
 class StudentsController < ApplicationController
+	
+	before_action :set_student, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@students = Student.all
 	end
 
 	def show
-		@students = Student.find(params[:id])
 	end
 
 	def new
@@ -12,15 +14,29 @@ class StudentsController < ApplicationController
 	end
 
 	def create
+		Student.create(params_student)
+		redirect_to students_path
 	end
 
 	def edit
-		@students = Student.find(params[:id])
 	end
 
 	def update
+		@students.update(params_student)
+		redirect_to student_path
 	end
 
 	def destroy
+		@students.destroy
+		redirect_to students_path
+	end
+
+	private
+	def params_student
+		params.require(:student).permit(:name, :ra)
+	end
+
+	def set_student
+		@students = Student.find(params[:id])
 	end
 end
